b91b1b0e9946cda357e1a4142dea96e8312759cf
alesha reed <aleshak83@gmail.com>
2019-10-12 13:07:02 -0500

    added hover function

--- src/javascripts/components/grid/grid.js
+++ src/javascripts/components/grid/grid.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import 'bootstrap';
 import './grid.scss';
 import utilities from '../../helpers/utilities';
@@ -5,25 +6,45 @@ import planets from '../data/planets';
 
 const makeGrid = () => {
   const planet = planets.getPlanets();
-  let domString = ` <h1 class="solarPageTitle">Solar System</h1>
-  <div class="card-group">`;
+  let domString = `
+    <h1 class="solarPageTitle">Solar System</h1>
+      <div class="planetgroup row d-flex">`;
   for (let i = 0; i < planet.length; i += 1) {
     domString += `
-    <div class="card planetCards">
-    <img class="card-img-top" src=${planet[i].image}>
-    <div class="card-body">
-      <h5 class="card-title">${planet[i].name}</h5>
-      <p class="card-text">Description: ${planet[i].description}</p>
-      <p class="card-text">Gas planet: ${planet[i].isGasPlanet}</p>
-      <p class="card-text">Number of Moons: ${planet[i].numberOfMoons}</p>
-      <p class="card-text"> Largest Moon : ${planet[i].nameOfLargestMoon}</p>
-      </div>
-      </div>
-  </div>`;
+        <div class="card planetCards">
+          <img class="card-img-top" src=${planet[i].image}>
+            <div class="card-body">
+              <h5 class="card-title">${planet[i].name}</h5>
+              <p class="card-text d-none">Description: ${planet[i].description}</p>
+              <p class="card-text d-none">Gas planet: ${planet[i].isGasPlanet}</p>
+              <p class="card-text d-none">Number of Moons: ${planet[i].numberOfMoons}</p>
+              <p class="card-text d-none"> Largest Moon : ${planet[i].nameOfLargestMoon}</p>
+            </div>
+           </div>
+          </div>
+          `;
   }
-  domString += '</div>';
+  domString += '</div></div>';
   utilities.printToDom('planetsHere', domString);
 };
 
+const hide = () => {
+  $('.card-img-top').hide();
+  $('.card-text').hide();
+};
 
-export default { makeGrid };
+const hoverFun = () => {
+  $('.planetCards').mouseover((e) => {
+    const card = $(e.target);
+    card.find('.card-img-top').show();
+    card.find('.card-title').hide();
+  });
+  $('.planetCards').mouseleave((e) => {
+    const card = $(e.target);
+    card.find('.card-img-top').hide();
+    card.find('.card-title').show();
+  });
+};
+
+
+export default { makeGrid, hoverFun, hide };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -4,5 +4,7 @@ import grid from './components/grid/grid';
 
 const init = () => {
   grid.makeGrid();
+  grid.hide();
+  grid.hoverFun();
 };
 init();
